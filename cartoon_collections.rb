91ac9803402102057ea024279bdaf8dab828d0e2
Paul M <paulmcadory@gmail.com>
2020-01-01 22:41:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   array.map! { |call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  # Your cod
 end
 
 def find_the_cheese# code an argument here
