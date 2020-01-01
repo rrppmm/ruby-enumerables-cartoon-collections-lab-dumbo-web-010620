74e16bd09c9090e60ecd5d003f68a438c2df0cf3
Paul M <paulmcadory@gmail.com>
2020-01-01 22:49:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types) === true
+  array.find array.include?(cheese_types) === true
     
 end
