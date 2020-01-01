e62a92a62a47c8f6628c8f4e34eef5087b3c8e7a
Paul M <paulmcadory@gmail.com>
2020-01-01 22:39:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map { |call| 
+  array.map { |call| call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
